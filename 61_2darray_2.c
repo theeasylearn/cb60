deee0cf6d30f793a4a451bdb1892c1b719cec8e7
theeasylearn <theeasylearn@gmail.com>
2026-08-07 13:07:33 +0530

    2darray

--- 61_2darray_2.c
+++ 61_2darray_2.c
@@ -6,7 +6,8 @@ void main()
     // create 2d array
     // datatype array[row][column]
     int sales[ROW][COLUMN];
-    int r, c;
+    int r, c,total=0;
+    float average;
     for (r = 0; r < ROW; r++)
     {
         printf("Enter input for %d dish\n", r + 1);
@@ -23,7 +24,10 @@ void main()
         for (c = 0; c < COLUMN; c++)
         {
             printf("[%5d]",sales[r][c]);
+            total = total + sales[r][c];
         }
         printf("\n");
     }
+    average = (float)total / (ROW * COLUMN);
+    printf("total = %d average = %f",total,average);
 }
